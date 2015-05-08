26c56ada2e9bfd267ee6877a3e396ff471f03e50
오후 취업반 < @ITbank31-PC>
2015-05-08 20:06:15 +0900

    second..

--- hello/src/hello/hello.java
+++ hello/src/hello/hello.java
@@ -1,5 +1,18 @@
 package hello;
 
 public class hello {
-		//sdafsaf
+		public static void main(String[] args) {
+			
+			for (int i = 2; i < 10; i += 4) {  
+				 		for (int j = 1; j < 10; j++) {  
+				 			System.out.println(  
+									i+"X"+j+"="+(i*j)+"\t"  
+									+(i+1)+"X"+j+"="+((i + 1)*j)+"\t"  
+				 						+(i+2)+"X"+j+"="+((i+2)*j)+"\t"  
+				 						+(i+3)+"X"+j+"="+((i+3)*j)+"\t");  
+				 			}  
+				 		System.out.println();  
+				 		}  
+
+		}
 }
