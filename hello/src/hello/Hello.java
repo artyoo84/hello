6009bf3e71153fc48e50341cdcd42bd73e9bf96c
오후 취업반 < @ITbank31-PC>
2015-05-08 21:35:29 +0900

    구구단

--- hello/src/hello/Hello.java
+++ hello/src/hello/Hello.java
@@ -2,22 +2,22 @@ package hello;
 
 public class Hello {
 	public static void main(String[] args) {
-			
+
 		int a;
 		int b;
-		
-			for(b=1;b<10;b++){
-				for(a=2; a<6;a++){
-					System.out.print(a+"x"+b+"="+a*b+"\t");
-				}
-				System.out.println("");
+
+		for (b = 1; b < 10; b++) {
+			for (a = 2; a < 6; a++) {
+				System.out.print(a + "x" + b + "=" + a * b + "\t");
 			}
-			System.out.println("\r");
-			for(b=1;b<10;b++){
-				for(a=6; a<10;a++){
-					System.out.print(a+"x"+b+"="+a*b+"\t");
-				}
-				System.out.println("");
+			System.out.println("");
+		}
+		System.out.println("\r");
+		for (b = 1; b < 10; b++) {
+			for (a = 6; a < 10; a++) {
+				System.out.print(a + "x" + b + "=" + a * b + "\t");
 			}
+			System.out.println("");
+		}
 	}
 }
